e79d177076c1e964cadc004dbbad3874bd5af43e
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-08-10 22:25:32 +0300

    Update SiteSimpluAwsInterface.java

--- SiteSimplu/src/main/java/com/sitesimplu/awsinterface/SiteSimpluAwsInterface.java
+++ SiteSimplu/src/main/java/com/sitesimplu/awsinterface/SiteSimpluAwsInterface.java
@@ -6,7 +6,6 @@ public class SiteSimpluAwsInterface {
 	private String accessKey;
 	
 	public SiteSimpluAwsInterface() {
-		accessKey = "AKIA3Q2X3H2SXOVHQ3KY";
-		secretAccessKey="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";
+
 	}
 }
